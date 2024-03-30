226b4194325870fc9fc9d7ee3fbd7d39495ad32c
Miguel Marmelete <20431@stu.ipbeja.pt>
2024-03-30 12:21:11 +0000

    stb tested

--- mandelbrot/src/main.c
+++ mandelbrot/src/main.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
-int main(){
-    printf("as");
+#include <stdlib.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main() {
+    // dimensões da imagem
+    int width = 640;
+    int height = 480;
+    int channels = 3; // RGB
+
+   
+    unsigned char *image = malloc(width * height * channels);
+    if (image == NULL) {
+        printf("Erro ao alocar memória para a nova imagem.\n");
+        return 1;
+    }
+
+    
+    memset(image, 0, width * height * channels);
+
+
+
+    //guardar imagem
+    if (!stbi_write_jpg("../images/mandelbrot.jpg", width, height, channels, image, 100)) {
+        printf("Erro ao salvar a nova imagem.\n");
+        free(image);
+        return 1;
+    }
+
+    free(image);
     return 0;
 }
